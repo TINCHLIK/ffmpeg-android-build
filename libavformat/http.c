TINCHLIK/ffmpeg-android-build

commit 9852c9ff180631d5b197a756e9be33964213c27a

lavf/http: Reset compressed header flag, fix http 302 request
Fixes ticket #6404.
Fixes ticket #6666.

Signed-off-by: Jacek Jendrzej <satbaby@kawaii.com>

libavformat/http.c
@@ -1248,6 +1248,7 @@ static int http_connect(URLContext *h, const char *path, const char *local_path,
     s->willclose        = 0;
     s->end_chunked_post = 0;
     s->end_header       = 0;
+    s->compressed       = 0;
     if (post && !s->post_data && !send_expect_100) {
         /* Pretend that it did work. We didn't read any header yet, since
          * we've still to send the POST data, but the code calling this

commit ee9e47ab1d9fbe73a0aaf961f003e9e71d388f65

lavf/http: fix compilation without zlib
Regression since 9852c9ff180631d5b197a756e9be33964213c27a.

libavformat/http.c
@@ -1248,7 +1248,9 @@ static int http_connect(URLContext *h, const char *path, const char *local_path,
     s->willclose        = 0;
     s->end_chunked_post = 0;
     s->end_header       = 0;
+#if CONFIG_ZLIB
     s->compressed       = 0;
+#endif
     if (post && !s->post_data && !send_expect_100) {
         /* Pretend that it did work. We didn't read any header yet, since
          * we've still to send the POST data, but the code calling this