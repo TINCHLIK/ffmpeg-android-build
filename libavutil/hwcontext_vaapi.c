TINCHLIK/ffmpeg-android-build

commit ba0a1094861fe65ce1dfbcc14492b689086164a7

hwcontext_vaapi: Add an assert in vaapi_map_from_drm()
Every fourcc in vaapi_drm_format_map should be in
vaapi_format_map, so add an assert to ensure we have the right maps.

Signed-off-by: Haihao Xiang <haihao.xiang@intel.com>

libavutil/hwcontext_vaapi.c
@@ -1028,6 +1028,8 @@ static int vaapi_map_from_drm(AVHWFramesContext *src_fc, AVFrame *dst,
             va_rt_format = vaapi_format_map[i].rt_format;
     }
 
+    av_assert0(i < FF_ARRAY_ELEMS(vaapi_format_map));
+
     buffer_handle = desc->objects[0].fd;
     buffer_desc.pixel_format = va_fourcc;
     buffer_desc.width        = src_fc->width;

commit 5044402e77c942ab68fbf8df0b28022df6a29055

hwcontext_vaapi: Fix mapping from DRM
This was broken by ba0a1094861fe65ce1dfbcc14492b689086164a7, which added
an assert that always failed.

libavutil/hwcontext_vaapi.c
@@ -1024,8 +1024,10 @@ static int vaapi_map_from_drm(AVHWFramesContext *src_fc, AVFrame *dst,
            "%08x.\n", desc->objects[0].fd, va_fourcc);
 
     for (i = 0; i < FF_ARRAY_ELEMS(vaapi_format_map); i++) {
-        if (vaapi_format_map[i].fourcc == va_fourcc)
+        if (vaapi_format_map[i].fourcc == va_fourcc) {
             va_rt_format = vaapi_format_map[i].rt_format;
+            break;
+        }
     }
 
     av_assert0(i < FF_ARRAY_ELEMS(vaapi_format_map));