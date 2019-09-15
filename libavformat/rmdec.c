TINCHLIK/ffmpeg-android-build

commit 51f5c6501f2c6d1c6675138af18360fb7d9da1d8

avformat/rmdec.c: fix left shift of negative value in rm_sync()
Fixes ticket 8143.

Reviewed-by: Andreas Rheinhardt <andreas.rheinhardt@gmail.com>
Signed-off-by: James Almer <jamrial@gmail.com>

libavformat/rmdec.c
@@ -724,8 +724,8 @@ static int rm_sync(AVFormatContext *s, int64_t *timestamp, int *flags, int *stre
 
             num = avio_rb16(pb);
             *timestamp = avio_rb32(pb);
-            mlti_id = (avio_r8(pb)>>1)-1<<16;
-            mlti_id = FFMAX(mlti_id, 0);
+            mlti_id = avio_r8((pb) >> 1) - 1;
+            mlti_id = FFMAX(mlti_id, 0) << 16;
             *flags = avio_r8(pb); /* flags */
         }
         for(i=0;i<s->nb_streams;i++) {

commit 046946554d023a27655e23095ca55aa470f7ce59

avformat/rmdec.c: fix brackets around arguments
Regression since 51f5c6501f

Signed-off-by: James Almer <jamrial@gmail.com>

libavformat/rmdec.c
@@ -724,7 +724,7 @@ static int rm_sync(AVFormatContext *s, int64_t *timestamp, int *flags, int *stre
 
             num = avio_rb16(pb);
             *timestamp = avio_rb32(pb);
-            mlti_id = avio_r8((pb) >> 1) - 1;
+            mlti_id = (avio_r8(pb) >> 1) - 1;
             mlti_id = FFMAX(mlti_id, 0) << 16;
             *flags = avio_r8(pb); /* flags */
         }