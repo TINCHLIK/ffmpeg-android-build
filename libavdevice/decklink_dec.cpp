TINCHLIK/ffmpeg-android-build

commit 135e77252481a3bd3310879009f9142d6a994cb1

avdevice: use the buffer_size_t typedef where required
Signed-off-by: James Almer <jamrial@gmail.com>

libavdevice/decklink_dec.cpp
@@ -1437,7 +1437,7 @@ int ff_decklink_read_packet(AVFormatContext *avctx, AVPacket *pkt)
     avpacket_queue_get(&ctx->queue, pkt, 1);
 
     if (ctx->tc_format && !(av_dict_get(ctx->video_st->metadata, "timecode", NULL, 0))) {
-        int size;
+        buffer_size_t size;
         const uint8_t *side_metadata = av_packet_get_side_data(pkt, AV_PKT_DATA_STRINGS_METADATA, &size);
         if (side_metadata) {
            if (av_packet_unpack_dictionary(side_metadata, size, &ctx->video_st->metadata) < 0)

libavdevice/decklink_enc.cpp
@@ -312,7 +312,8 @@ static void construct_cc(AVFormatContext *avctx, struct decklink_ctx *ctx,
     uint16_t *cdp_words;
     uint16_t len;
     uint8_t cc_count;
-    int size, ret, i;
+    buffer_size_t size;
+    int ret, i;
 
     const uint8_t *data = av_packet_get_side_data(pkt, AV_PKT_DATA_A53_CC, &size);
     if (!data)

libavdevice/xcbgrab.c
@@ -236,7 +236,7 @@ static void free_shm_buffer(void *opaque, uint8_t *data)
     shmdt(data);
 }
 
-static AVBufferRef *allocate_shm_buffer(void *opaque, int size)
+static AVBufferRef *allocate_shm_buffer(void *opaque, buffer_size_t size)
 {
     xcb_connection_t *conn = opaque;
     xcb_shm_seg_t segment;

commit a2611db8c784e335b3aa245268d3137b66cb3b1f

avdevice/decklink: add missing include for buffer_size_t
Should fix compilation broken in 135e77252481a3bd3310879009f9142d6a994cb1.

Reviewed-by: Marton Balint <cus@passwd.hu>
Signed-off-by: James Almer <jamrial@gmail.com>

libavdevice/decklink_dec.cpp
@@ -39,6 +39,7 @@ extern "C" {
 #include "libavutil/avassert.h"
 #include "libavutil/avutil.h"
 #include "libavutil/common.h"
+#include "libavutil/internal.h"
 #include "libavutil/imgutils.h"
 #include "libavutil/intreadwrite.h"
 #include "libavutil/time.h"

libavdevice/decklink_enc.cpp
@@ -32,6 +32,7 @@ extern "C" {
 
 extern "C" {
 #include "libavformat/avformat.h"
+#include "libavutil/internal.h"
 #include "libavutil/imgutils.h"
 #include "avdevice.h"
 }