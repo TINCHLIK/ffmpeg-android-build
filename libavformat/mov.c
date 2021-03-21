TINCHLIK/ffmpeg-android-build

commit 21b8539e8d1d5de61de9bd7b52c1a31b1398a5b7

avformat/mov: Ignore tags with invalid size during probing
Reviewed-by: Paul B Mahol <onemda@gmail.com>
Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

libavformat/mov.c
@@ -7113,14 +7113,20 @@ static int mov_probe(const AVProbeData *p)
     offset = 0;
     for (;;) {
         int64_t size;
+        int minsize = 8;
         /* ignore invalid offset */
         if ((offset + 8) > (unsigned int)p->buf_size)
             break;
         size = AV_RB32(p->buf + offset);
         if (size == 1 && offset + 16 > (unsigned int)p->buf_size) {
             size = AV_RB64(p->buf+offset + 8);
+            minsize = 16;
         } else if (size == 0) {
             size = p->buf_size - offset;
+        }
+        if (size < minsize) {
+            offset += 4;
+            continue;
         }
         tag = AV_RL32(p->buf + offset + 4);
         switch(tag) {
@@ -7131,9 +7137,7 @@ static int mov_probe(const AVProbeData *p)
         case MKTAG('p','n','o','t'): /* detect movs with preview pics like ew.mov and april.mov */
         case MKTAG('u','d','t','a'): /* Packet Video PVAuthor adds this and a lot of more junk */
         case MKTAG('f','t','y','p'):
-            if (size < 8) {
-                score = FFMAX(score, AVPROBE_SCORE_EXTENSION);
-            } else if (tag == MKTAG('f','t','y','p') &&
+            if (tag == MKTAG('f','t','y','p') &&
                        (   AV_RL32(p->buf + offset + 8) == MKTAG('j','p','2',' ')
                         || AV_RL32(p->buf + offset + 8) == MKTAG('j','p','x',' ')
                     )) {
@@ -7158,7 +7162,7 @@ static int mov_probe(const AVProbeData *p)
             score  = FFMAX(score, AVPROBE_SCORE_EXTENSION);
             break;
         }
-        offset = FFMAX(4, size) + offset;
+        offset += size;
     }
     if (score > AVPROBE_SCORE_MAX - 50 && moov_offset != -1) {
         /* moov atom in the header - we should make sure that this is not a

commit c6bfb8eeaee3baf59594f6f40ad46cb516b3ca75

avformat/mov: Fix extended atom size buffer length check
When extended atom size support was added to probing in
c308b68d4eb9f0e36fa20154999813a61b2d01fc, the buffer
size check was backwards, but probing continued to work
because there was no minimum size check yet, so despite
size being 1 on these atoms, and failing to read the 64-bit
size, the tag was still correctly read.

When 21b8539e8d1d5de61de9bd7b52c1a31b1398a5b7 introduced a
minimum size check, this exposed the bug, and broke probing
any files with extended atom sizes, such as entirely valid
large files that start whith mdat atoms.

Signed-off-by: Derek Buitenhuis <derek.buitenhuis@gmail.com>

libavformat/mov.c
@@ -7121,7 +7121,7 @@ static int mov_probe(const AVProbeData *p)
         if ((offset + 8) > (unsigned int)p->buf_size)
             break;
         size = AV_RB32(p->buf + offset);
-        if (size == 1 && offset + 16 > (unsigned int)p->buf_size) {
+        if (size == 1 && offset + 16 <= (unsigned int)p->buf_size) {
             size = AV_RB64(p->buf+offset + 8);
             minsize = 16;
         } else if (size == 0) {