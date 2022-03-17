TINCHLIK/ffmpeg-android-build

commit 6c706d5261677e1fc9ff79847e0873ea678d0603

boa: convert to new channel layout API
Signed-off-by: James Almer <jamrial@gmail.com>

libavformat/boadec.c
@@ -55,15 +55,16 @@ static int read_header(AVFormatContext *s)
     avio_rl32(s->pb);
     avio_rl32(s->pb);
     st->codecpar->sample_rate = avio_rl32(s->pb);
-    st->codecpar->channels    = avio_rl32(s->pb);
-    if (st->codecpar->channels > FF_SANE_NB_CHANNELS || st->codecpar->channels <= 0)
+    st->codecpar->ch_layout.nb_channels    = avio_rl32(s->pb);
+    if (st->codecpar->ch_layout.nb_channels > FF_SANE_NB_CHANNELS ||
+        st->codecpar->ch_layout.order <= 0)
         return AVERROR(ENOSYS);
     ffformatcontext(s)->data_offset = data_offset = avio_rl32(s->pb);
     avio_r8(s->pb);
     st->codecpar->block_align = avio_rl32(s->pb);
     if (st->codecpar->block_align > INT_MAX / FF_SANE_NB_CHANNELS || st->codecpar->block_align <= 0)
         return AVERROR_INVALIDDATA;
-    st->codecpar->block_align *= st->codecpar->channels;
+    st->codecpar->block_align *= st->codecpar->ch_layout.nb_channels;
 
     avio_seek(s->pb, data_offset, SEEK_SET);
 

commit b2a32fa24677b03d3a5f56a4eefb8717e7cd8a7b

avformat/boadec: fix check for invalid channel count
Regression since 6c706d5261677e1fc9ff79847e0873ea678d0603.

Signed-off-by: James Almer <jamrial@gmail.com>

libavformat/boadec.c
@@ -57,7 +57,7 @@ static int read_header(AVFormatContext *s)
     st->codecpar->sample_rate = avio_rl32(s->pb);
     st->codecpar->ch_layout.nb_channels    = avio_rl32(s->pb);
     if (st->codecpar->ch_layout.nb_channels > FF_SANE_NB_CHANNELS ||
-        st->codecpar->ch_layout.order <= 0)
+        st->codecpar->ch_layout.nb_channels <= 0)
         return AVERROR(ENOSYS);
     ffformatcontext(s)->data_offset = data_offset = avio_rl32(s->pb);
     avio_r8(s->pb);