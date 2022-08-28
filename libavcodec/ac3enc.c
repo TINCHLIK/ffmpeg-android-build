TINCHLIK/ffmpeg-android-build

commit 62f2d23a1f8d7ffd4998989c42570dc4bef7e2ee

avcodec/internal: Move ff_samples_to_time_base() to encode.h
It is only used by encoders; in fact, AVCodecContext.time_base
is only used by encoders, so it is only useful for encoders.

Also constify the AVCodecContext parameter in it.

Also fixup the other headers a bit while removing now unnecessary
internal.h inclusions.

Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@outlook.com>

libavcodec/ac3enc.c
@@ -30,7 +30,6 @@
 
 #include "libavutil/attributes.h"
 #include "libavutil/avassert.h"
-#include "libavutil/avstring.h"
 #include "libavutil/channel_layout.h"
 #include "libavutil/crc.h"
 #include "libavutil/internal.h"
@@ -41,7 +40,6 @@
 #include "codec_internal.h"
 #include "config_components.h"
 #include "encode.h"
-#include "internal.h"
 #include "me_cmp.h"
 #include "put_bits.h"
 #include "audiodsp.h"

libavcodec/audio_frame_queue.c
@@ -22,7 +22,7 @@
 #include "libavutil/attributes.h"
 #include "libavutil/common.h"
 #include "audio_frame_queue.h"
-#include "internal.h"
+#include "encode.h"
 #include "libavutil/avassert.h"
 
 av_cold void ff_af_queue_init(AVCodecContext *avctx, AudioFrameQueue *afq)

libavcodec/encode.h
@@ -78,4 +78,16 @@ int ff_encode_preinit(AVCodecContext *avctx);
 int ff_encode_encode_cb(AVCodecContext *avctx, AVPacket *avpkt,
                         const AVFrame *frame, int *got_packet);
 
+/**
+ * Rescale from sample rate to AVCodecContext.time_base.
+ */
+static av_always_inline int64_t ff_samples_to_time_base(const AVCodecContext *avctx,
+                                                        int64_t samples)
+{
+    if (samples == AV_NOPTS_VALUE)
+        return AV_NOPTS_VALUE;
+    return av_rescale_q(samples, (AVRational){ 1, avctx->sample_rate },
+                        avctx->time_base);
+}
+
 #endif /* AVCODEC_ENCODE_H */

libavcodec/flacenc.c
@@ -32,7 +32,6 @@
 #include "encode.h"
 #include "put_bits.h"
 #include "put_golomb.h"
-#include "internal.h"
 #include "lpc.h"
 #include "flac.h"
 #include "flacdata.h"

libavcodec/g722enc.c
@@ -32,7 +32,6 @@
 #include "avcodec.h"
 #include "codec_internal.h"
 #include "encode.h"
-#include "internal.h"
 #include "g722.h"
 #include "libavutil/common.h"
 

libavcodec/internal.h
@@ -182,18 +182,6 @@ void ff_color_frame(AVFrame *frame, const int color[4]);
 #define FF_MAX_EXTRADATA_SIZE ((1 << 28) - AV_INPUT_BUFFER_PADDING_SIZE)
 
 /**
- * Rescale from sample rate to AVCodecContext.time_base.
- */
-static av_always_inline int64_t ff_samples_to_time_base(AVCodecContext *avctx,
-                                                        int64_t samples)
-{
-    if(samples == AV_NOPTS_VALUE)
-        return AV_NOPTS_VALUE;
-    return av_rescale_q(samples, (AVRational){ 1, avctx->sample_rate },
-                        avctx->time_base);
-}
-
-/**
  * 2^(x) for integer x
  * @return correctly rounded float
  */

libavcodec/libtwolame.c
@@ -33,7 +33,6 @@
 #include "avcodec.h"
 #include "codec_internal.h"
 #include "encode.h"
-#include "internal.h"
 #include "mpegaudio.h"
 
 typedef struct TWOLAMEContext {

libavcodec/libvo-amrwbenc.c
@@ -29,7 +29,6 @@
 #include "avcodec.h"
 #include "codec_internal.h"
 #include "encode.h"
-#include "internal.h"
 
 #define MAX_PACKET_SIZE  (1 + (477 + 7) / 8)
 

commit cbf1dcce6456f7af559ab24ae29f987a8f1315d3

avcodec/ac3enc: Add missing header
Needed for code under #ifdef DEBUG; broken in commit
62f2d23a1f8d7ffd4998989c42570dc4bef7e2ee.

Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@outlook.com>

libavcodec/ac3enc.c
@@ -30,6 +30,7 @@
 
 #include "libavutil/attributes.h"
 #include "libavutil/avassert.h"
+#include "libavutil/avstring.h"
 #include "libavutil/channel_layout.h"
 #include "libavutil/crc.h"
 #include "libavutil/internal.h"