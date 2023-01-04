TINCHLIK/ffmpeg-android-build

commit 727a78c003a50f1d845baf09e56b6233d94a63fd

avformat/oggenc: ignore empty packets
Some encoders, like flac, can send side data only packets at the end.
Eventually, said extradata update should ideally be used to update the header
when writting to seekable output, but for now, ignore them.

Should fix the undefined behavior of passing NULL to memcpy().

Signed-off-by: James Almer <jamrial@gmail.com>

libavformat/oggenc.c
@@ -687,7 +687,7 @@ static int ogg_write_packet(AVFormatContext *s, AVPacket *pkt)
 {
     int i;
 
-    if (pkt)
+    if (pkt && pkt->size)
         return ogg_write_packet_internal(s, pkt);
 
     for (i = 0; i < s->nb_streams; i++) {

tests/ref/lavf/ogg
@@ -1,3 +1,3 @@
-81b9366cacb23644c2803585dced9996 *tests/data/lavf/lavf.ogg
+507a906a705d16f3a3b0c4114c738110 *tests/data/lavf/lavf.ogg
 13516 tests/data/lavf/lavf.ogg
 tests/data/lavf/lavf.ogg CRC=0x3a1da17e

commit b3cf8b34cc593aa8cb47e3447e2c295c9d720244

avformat/oggenc: don't flush twice when the last packet is side data only
Commit 727a78c003 accidentally made side data only packets be handled like a
flush request. Fix this regression by effectively ignoring them as was the
original intention.

Signed-off-by: James Almer <jamrial@gmail.com>

libavformat/oggenc.c
@@ -687,8 +687,8 @@ static int ogg_write_packet(AVFormatContext *s, AVPacket *pkt)
 {
     int i;
 
-    if (pkt && pkt->size)
-        return ogg_write_packet_internal(s, pkt);
+    if (pkt)
+        return pkt->size ? ogg_write_packet_internal(s, pkt) : 0;
 
     for (i = 0; i < s->nb_streams; i++) {
         OGGStreamContext *oggstream = s->streams[i]->priv_data;

tests/ref/lavf/ogg
@@ -1,3 +1,3 @@
-507a906a705d16f3a3b0c4114c738110 *tests/data/lavf/lavf.ogg
+81b9366cacb23644c2803585dced9996 *tests/data/lavf/lavf.ogg
 13516 tests/data/lavf/lavf.ogg
 tests/data/lavf/lavf.ogg CRC=0x3a1da17e