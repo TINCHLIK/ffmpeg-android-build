TINCHLIK/ffmpeg-android-build

commit b1c022cae78b8a02337c78c957b79b602848ec96

hw_base_encode: Free pictures on close
Fixes leaking recon surfaces with VAAPI.

libavcodec/hw_base_encode.c
@@ -804,6 +804,11 @@ int ff_hw_base_encode_init(AVCodecContext *avctx, FFHWBaseEncodeContext *ctx)
 
 int ff_hw_base_encode_close(FFHWBaseEncodeContext *ctx)
 {
+    FFHWBaseEncodePicture *pic;
+
+    for (pic = ctx->pic_start; pic; pic = pic->next)
+        base_encode_pic_free(pic);
+
     av_fifo_freep2(&ctx->encode_fifo);
 
     av_frame_free(&ctx->frame);

commit ed30aa37119169e31d23cb0c350480eb5c65f23a

avcodec/hw_base_encode: fix use after free on close
The way the linked list of images was freed caused a
use after free, by accessing pic->next after pic was
already freed.

Regression from b1c022cae78b8a02337c78c957b79b602848ec96

Fix CID1633236

libavcodec/hw_base_encode.c
@@ -804,10 +804,10 @@ int ff_hw_base_encode_init(AVCodecContext *avctx, FFHWBaseEncodeContext *ctx)
 
 int ff_hw_base_encode_close(FFHWBaseEncodeContext *ctx)
 {
-    FFHWBaseEncodePicture *pic;
-
-    for (pic = ctx->pic_start; pic; pic = pic->next)
+    for (FFHWBaseEncodePicture *pic = ctx->pic_start, *next_pic = pic; pic; pic = next_pic) {
+        next_pic = pic->next;
         base_encode_pic_free(pic);
+    }
 
     av_fifo_freep2(&ctx->encode_fifo);
 