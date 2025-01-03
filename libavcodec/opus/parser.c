TINCHLIK/ffmpeg-android-build

commit e0b247ba78e5406d85f2057a05a92e1ce77e176f

avcodec/opus/parser: set sample rate
Ensures it's set on scenarios where a parser is requested but no decoder is
present.

Signed-off-by: James Almer <jamrial@gmail.com>

libavcodec/opus/parser.c
@@ -170,9 +170,14 @@ static int opus_parse(AVCodecParserContext *ctx, AVCodecContext *avctx,
 {
     OpusParserContext *s = ctx->priv_data;
     ParseContext *pc    = &s->pc;
-    int next, header_len;
+    int next, header_len = 0;
 
-    next = opus_find_frame_end(ctx, avctx, buf, buf_size, &header_len);
+    avctx->sample_rate = 48000;
+
+    if (ctx->flags & PARSER_FLAG_COMPLETE_FRAMES)
+        next = buf_size;
+    else {
+        next = opus_find_frame_end(ctx, avctx, buf, buf_size, &header_len);
 
     if (s->ts_framing && next != AVERROR_INVALIDDATA &&
         ff_combine_frame(pc, next, &buf, &buf_size) < 0) {
@@ -185,6 +190,7 @@ static int opus_parse(AVCodecParserContext *ctx, AVCodecContext *avctx,
         *poutbuf      = NULL;
         *poutbuf_size = 0;
         return buf_size;
+    }
     }
 
     *poutbuf      = buf + header_len;

commit 65023cd5b38fb126a5e08ae8f5e43da692b4825f

avcodec/opus/parser: set duration when complete frames are fed
Fixes a regression since e0b247ba78e5406d85f2057a05a92e1ce77e176f.

Signed-off-by: James Almer <jamrial@gmail.com>

libavcodec/opus/parser.c
@@ -78,6 +78,21 @@ static const uint8_t *parse_opus_ts_header(const uint8_t *start, int *payload_le
     return buf + bytestream2_tell(&gb);
 }
 
+static int set_frame_duration(AVCodecParserContext *ctx, AVCodecContext *avctx,
+                              const uint8_t *buf, int buf_size)
+{
+    OpusParserContext *s = ctx->priv_data;
+
+    if (ff_opus_parse_packet(&s->pkt, buf, buf_size, s->ctx.nb_streams > 1) < 0) {
+        av_log(avctx, AV_LOG_ERROR, "Error parsing Opus packet header.\n");
+        return AVERROR_INVALIDDATA;
+    }
+
+    ctx->duration = s->pkt.frame_count * s->pkt.frame_duration;
+
+    return 0;
+}
+
 /**
  * Find the end of the current frame in the bitstream.
  * @return the position of the first byte of the next frame, or -1
@@ -126,25 +141,12 @@ static int opus_find_frame_end(AVCodecParserContext *ctx, AVCodecContext *avctx,
     if (!s->ts_framing)
         payload_len = buf_size;
 
-    if (avctx->extradata && !s->extradata_parsed) {
-        ret = ff_opus_parse_extradata(avctx, &s->ctx);
-        if (ret < 0) {
-            av_log(avctx, AV_LOG_ERROR, "Error parsing Ogg extradata.\n");
-            return AVERROR_INVALIDDATA;
-        }
-        av_freep(&s->ctx.channel_maps);
-        s->extradata_parsed = 1;
-    }
-
     if (payload_len <= buf_size && (!s->ts_framing || start_found)) {
-        ret = ff_opus_parse_packet(&s->pkt, payload, payload_len, s->ctx.nb_streams > 1);
+        ret = set_frame_duration(ctx, avctx, payload, payload_len);
         if (ret < 0) {
-            av_log(avctx, AV_LOG_ERROR, "Error parsing Opus packet header.\n");
             pc->frame_start_found = 0;
             return AVERROR_INVALIDDATA;
         }
-
-        ctx->duration = s->pkt.frame_count * s->pkt.frame_duration;
     }
 
     if (s->ts_framing) {
@@ -174,9 +176,26 @@ static int opus_parse(AVCodecParserContext *ctx, AVCodecContext *avctx,
 
     avctx->sample_rate = 48000;
 
-    if (ctx->flags & PARSER_FLAG_COMPLETE_FRAMES)
+    if (avctx->extradata && !s->extradata_parsed) {
+        if (ff_opus_parse_extradata(avctx, &s->ctx) < 0) {
+            av_log(avctx, AV_LOG_ERROR, "Error parsing Ogg extradata.\n");
+            *poutbuf      = NULL;
+            *poutbuf_size = 0;
+            return buf_size;
+        }
+        av_freep(&s->ctx.channel_maps);
+        s->extradata_parsed = 1;
+    }
+
+    if (ctx->flags & PARSER_FLAG_COMPLETE_FRAMES) {
         next = buf_size;
-    else {
+
+        if (set_frame_duration(ctx, avctx, buf, buf_size) < 0) {
+            *poutbuf      = NULL;
+            *poutbuf_size = 0;
+            return buf_size;
+        }
+    } else {
         next = opus_find_frame_end(ctx, avctx, buf, buf_size, &header_len);
 
         if (s->ts_framing && next != AVERROR_INVALIDDATA &&